b5b2be36093fa053f3b1770fed624a28b87b062d
Ryan Jones <ryanajonesdev@gmail.com>
2021-05-10 23:41:30 -0700

    added getThreads function to populate the board with threads

--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,7 @@ mongoose.set('useFindAndModify', false);
 const { Schema } = mongoose;
 
 const threadSchema = new Schema({
+  board: { type: String, required: true },
   text: { type: String, required: true },
   created_on: { type: Date, required: true, default: new Date().toUTCString() },
   bumped_on: { type: Date, required: true, default: new Date().toUTCString() },
@@ -19,21 +20,53 @@ const threadSchema = new Schema({
 const Threads = mongoose.model('threads', threadSchema);
 
 // Database new thread
-const newThread = (text, delete_password) => {
+const newThread = (board, text, delete_password) => {
   const saveThread = new Threads({
+    board,
     text,
     delete_password,
   });
   return saveThread.save();
 };
 
-module.exports = function (app) {
-  app.route('/api/threads/:board').post(async (req, res) => {
-    const { board, text, delete_password } = req.body;
-    const thread = await newThread(text, delete_password);
+// Database find all threads for specific board
+const getThreads = (board) =>
+  Threads.find({ board }).then((res) => {
+    const sortedThreads = res.sort((a, b) => a.created_on - b.created_on);
+    const reduceThreadCount = [];
+    sortedThreads.forEach((el, i) => {
+      if (i < 10) reduceThreadCount.push(el);
+    });
 
-    return res.redirect(`/b/${board}`);
+    const reduceRepliesCount = [];
+    reduceThreadCount.forEach((el, i) => {
+      el.replies.sort((a, b) => a.created_on - b.created_on);
+    });
+    /*  reduceThreadCount.forEach((el, i) => {
+      el.replies.forEach((reply, index) => {
+        if (index < 3) reduceRepliesCount.push(reply);
+      });
+    });
+    reduceThreadCount.forEach((el, i) => {
+      el.replies = reduceRepliesCount[i];
+    });
+ */
+    console.log(reduceThreadCount);
   });
+
+module.exports = function (app) {
+  app
+    .route('/api/threads/:board')
+    .post(async (req, res) => {
+      const { board, text, delete_password } = req.body;
+      const thread = await newThread(board, text, delete_password);
+      return res.redirect(`/b/${board}`);
+    })
+    .get(async (req, res) => {
+      const { board } = req.params;
+      const threads = await getThreads(board);
+      return res.json(threads);
+    });
 
   app.route('/api/replies/:board');
 };
